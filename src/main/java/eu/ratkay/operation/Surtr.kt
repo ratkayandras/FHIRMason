a27c71c58fdbe6bf592499f70c214a9c60bd71c9
ratkaya <ratkay.andras@gmail.com>
2024-01-03 16:52:20 +0100

    Refactor Surtr

--- src/main/java/eu/ratkay/operation/Surtr.kt
+++ src/main/java/eu/ratkay/operation/Surtr.kt
@@ -7,46 +7,33 @@ import org.hl7.fhir.r4.model.OperationOutcome
 import org.hl7.fhir.r4.model.Parameters
 import org.hl7.fhir.r4.model.Resource
 
-sealed class Surtr {
-
-    private val parameters: Parameters?
-    private val fatalError: OperationOutcome?
-
-    private constructor(parameters: Parameters) {
-        this.parameters = parameters
-        this.fatalError = null
-    }
-
-    private constructor(operationOutcome: OperationOutcome) {
-        this.parameters = null
-        this.fatalError = operationOutcome
-    }
+sealed class Surtr private constructor(private val parameters: Parameters) {
 
     companion object {
-        fun of(resource: Resource, name: String? = "resource"): Surtr {
+        fun of(resource: Resource, name: String = "resource"): Surtr {
             return if (resource is OperationOutcome && resource.hasIssue() && resource.hasError()) {
                 Error(resource)
             } else {
-                SuccessResource(name!!, resource)
+                SuccessResource(name, resource)
             }
         }
     }
 
-    val resource: IBaseResource?
+    val resource: IBaseResource
         get() = when (this) {
             is SuccessResource -> this.parameters
-            is Error -> this.fatalError
+            is Error -> this.operationOutcome
         }
 
     private data class SuccessResource(val name: String, val source: Resource): Surtr(source.toParameters(name))
-    private data class Error(val operationOutcome: OperationOutcome): Surtr(operationOutcome)
+    private data class Error(val operationOutcome: OperationOutcome): Surtr(Parameters())
 
     fun operate(lambda: () -> Resource): Surtr {
         return if (this is SuccessResource) of(lambda.invoke()) else this
     }
 
     fun operateCombined(lambda: () -> Resource): Surtr {
-        return if (this is SuccessResource) of(lambda.invoke()) combine this.parameters!! else this
+        return if (this is SuccessResource) of(lambda.invoke()) combine this.parameters else this
     }
 
     fun operateResource(lambda: (Resource) -> Resource): Surtr {
@@ -63,7 +50,7 @@ sealed class Surtr {
      */
     private infix fun combine(parameters: Parameters): Surtr {
         return if (this is SuccessResource) {
-            val newParams = parameters.parameter + this.parameters!!.parameter
+            val newParams = parameters.parameter + this.parameters.parameter
             of(Parameters().apply {
                 parameter = newParams
             })
